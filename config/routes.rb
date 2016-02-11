6e97ecebfa07c72661b4888dd8f8ab0110519df1
Gaztambide <francogaztambide@gmail.com>
2016-02-11 12:07:07 -0500

    use pg in record

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   get 'statuses', to: 'statuses#index', page: 1
 
-  get 'statuses/page/:age', to: 'statuses#index'
-  get 'statuses/index'
-
-  get 'statuses/show'
-
-  get 'statuses/new'
+  get 'statuses/page/:page', to: 'statuses#index'
+  # get 'statuses/index'
+  #
+  # get 'statuses/show'
+  #
+  # get 'statuses/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
